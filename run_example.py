5bc80fd4b9df55a80f82edfa1b1e55225a38556e
yimingdongdong <145199396+yimingdongdong@users.noreply.github.com>
2025-10-24 04:01:13 +0800

    Update run_example.py

--- run_example.py
+++ run_example.py
@@ -1,1 +1,12 @@
+import torch
+from DeepCas12a import Episgt, VisionTransformer
 
+data = Episgt('example/example_sequences.txt', num_epi_features=2, with_y=True)
+X, y = data.get_dataset()
+X = torch.tensor(X).unsqueeze(1)
+# 你也可以选择其他fold: fold_2, fold_3, ..., fold_9或进行集成预测
+model = VisionTransformer()
+model.load_state_dict(torch.load('trained_model/fold1.pth'))
+model.eval()
+with torch.no_grad():
+    predictions = model(X)
